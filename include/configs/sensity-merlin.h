d899d83ceb266181d3252a84b0bebe0f100d71ec
Matt Madison <mmadison@sensity.com>
2016-09-27 15:25:11 -0700

    sensity-merlin: turn off distro_bootcmd support
    as it is not needed on this platform.

--- include/configs/sensity-merlin.h
+++ include/configs/sensity-merlin.h
@@ -69,8 +69,65 @@
 /* General networking support */
 #define CONFIG_CMD_DHCP
 
+/* XXX - settings for now, will need to change */
+#define CONFIG_BOOT_RETRY_TIME		300
+#define CONFIG_BOOTDELAY		2
+#define CONFIG_BOOTCOUNT_ENV
+#define CONFIG_BOOTCOUNT_LIMIT
+#define CONFIG_BOOT_RETRY_TIME_MIN	10
+#define CONFIG_RESET_TO_RETRY
+#define CONFIG_DEFAULT_FDT_FILE		"tegra210-sensity-merlin.dtb"
+#define CONFIG_CONSOLE_DEV		"ttyS0"
+
+#define BOARD_BASE_BOOTARGS "ddr_die=2048@2048M ddr_die=2048@4096M section=256M memtype=0 vpr_resize " \
+	                    "usb_port_owner_info=0 lane_owner_info=0 emc_max_dvfs=0 no_console_suspend=1 " \
+	                    "maxcpus=4 usbcore.old_scheme_first=1 lp0_vec=${lp0_vec} nvdumper_reserved=${nvdumper_reserved} " \
+	                    "core_edp_mv=1125 core_edp_map=4000 gpt"
+
+#define BOARD_EXTRA_ENV_SETTINGS \
+	"bootcount=0\0" \
+	"upgrade_available=1\0" \
+	"bootlimit=3\0" \
+	"bootretry=" __stringify(CONFIG_BOOT_RETRY_TIME) "\0" \
+	"fdtfile=" CONFIG_DEFAULT_FDT_FILE "\0" \
+	"fdt_addr_r=0x82000000\0" \
+	"console=" CONFIG_CONSOLE_DEV "\0" \
+	"extra_bootargs=loglevel=3\0" \
+	"mmcpart=1\0" \
+	"mmcroot=/dev/mmcblk0p1\0" \
+	"mmcroot_eval=setenv mmcroot /dev/mmcblk0p${mmcpart}\0" \
+	"mmcpart_swap=if test ${mmcpart} -eq 2; then setenv mmcpart 1; else setenv mmcpart 2; fi; setenv bootcount 0; saveennv\0" \
+	"mmcargs=run mmcroot_eval; " \
+		"setenv bootargs console=${console},115200n8 " BOARD_BASE_BOOTARGS " root=${mmcroot} ro rootwait ${extra_bootargs}\0" \
+	"image=Image\0" \
+	"kernel_addr_r=" __stringify(CONFIG_LOADADDR) "\0" \
+	"loadimage=echo Loading kernel at ${kernel_addr_r}; ext2load mmc 0:${mmcpart} ${kernel_addr_r} /boot/${image}\0" \
+	"loadfdt=echo Loading FDT at ${fdt_addr_r}; ext2load mmc 0:${mmcpart} ${fdt_addr_r} /boot/${fdtfile}\0" \
+	"defaultfdt=ext2load mmc 0:${mmcpart} ${fdt_addr_r} /boot/" CONFIG_DEFAULT_FDT_FILE "\0" \
+	"mmcboot=echo Boot args: ${bootargs}; echo Booting from eMMC...; " \
+		"if run loadfdt; then booti ${kernel_addr_r} - ${fdt_addr_r}; " \
+			"else if run defaultfdt; then booti ${kernel_addr_r} - ${fdt_addr_r}; " \
+				"else echo FAIL: could not load FDT; " \
+		"fi; fi;\0" \
+	 "altbootcmd=run mmcpart_swap; run bootcmd\0"
+
 #include "tegra-common-usb-gadget.h"
 #include "tegra-common-post.h"
+/* Clear BOOTENV set by config_distro_bootcmd.h */
+#undef BOOTENV
+#define BOOTENV
+
+#undef CONFIG_BOOTCOMMAND
+#define CONFIG_BOOTCOMMAND \
+			"mmc dev 0; if mmc rescan; then " \
+				"run mmcargs; if run loadimage; then " \
+					"run mmcboot; " \
+				"else " \
+					"echo FAIL: could not find Linux kernel; " \
+				"fi; " \
+			"else " \
+				"echo FAIL: mmc rescan failed; " \
+			"fi"
 
 #define COUNTER_FREQUENCY	38400000
 
